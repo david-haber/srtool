f487daaca881abfe8764c4ef66e2593a7da04535
Oliver Wilkie <osw09@ic.ac.uk>
2012-11-24 10:08:56 +0000

    Added a helper function b32vtobool which is syntactically correct! (Tried and tested on z3)

--- src/srt/tool/SMTLIBConverter.java
+++ src/srt/tool/SMTLIBConverter.java
@@ -24,7 +24,8 @@ public class SMTLIBConverter {
 		exprConverter = new ExprToSmtlibVisitor();
 		query = new StringBuilder("(set-logic QF_BV)\n" +
 				"(declare-sort Int 0)\n"+
-				"(define-fun tobv32 ((p Bool)) (_ BitVec 32) (ite p (_ bv1 32) (_ bv0 32)))\n");
+				"(define-fun tobv32 ((p Bool)) (_ BitVec 32) (ite p (_ bv1 32) (_ bv0 32)))\n" +
+				"(define-fun bv32tobool ((b  (_ BitVec 32))) (Bool) (= b (_ bv1 32) ))\n");
 				//"(define-fun inttobv32 ((i Int))  (_ BitVec 32) (_ bv 32))\n");
 		// TODO: Define more functions above (for convenience), as needed.
 
