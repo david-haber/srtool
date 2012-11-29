88c1fd6a52bfac0b7c6f7b01a9f8628cf127a08b
Oliver Wilkie <osw09@imperial.ac.uk>
2012-11-29 18:23:31 +0000

    Fixed bug in bv32tobool

--- src/srt/tool/SMTLIBConverter.java
+++ src/srt/tool/SMTLIBConverter.java
@@ -26,7 +26,7 @@ public class SMTLIBConverter {
 		query = new StringBuilder("(set-logic QF_BV)\n" +
 				"(declare-sort Int 0)\n"+
 				"(define-fun tobv32 ((p Bool)) (_ BitVec 32) (ite p (_ bv1 32) (_ bv0 32)))\n" +
-				"(define-fun bv32tobool ((b  (_ BitVec 32))) (Bool) (= b (_ bv1 32) ))\n");
+				"(define-fun bv32tobool ((b  (_ BitVec 32))) (Bool) (not (= b (_ bv0 32) )))\n");
 		// TODO: Define more functions above (for convenience), as needed.
 
 		// TODO: Add constraints, add properties to check
